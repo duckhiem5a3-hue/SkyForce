72b0951a6a1c320ec1fffc77055f4bdc010653d3
duckhiem5a3-hue <duckhiem5a3@gmail.com>
2026-08-17 23:42:36 +0700

    tạo tính năng scroll cho LevelScene

--- src/main/java/com/nhom27/skyforce/scenes/LevelScene.java
+++ src/main/java/com/nhom27/skyforce/scenes/LevelScene.java
@@ -8,25 +8,15 @@ import com.nhom27.skyforce.main.Main;
 import com.nhom27.skyforce.ui.buttons.CustomButton;
 import com.nhom27.skyforce.utils.AssetManager;
 
+import javafx.animation.AnimationTimer;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.effect.BlurType;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.effect.Glow;
+import javafx.scene.effect.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -34,8 +24,19 @@ public class LevelScene {
     private static LevelScene instance;
     private Scene scene;
     private HBox levelCardsContainer;
-    
     private int selectedLevel = 1; // Mặc định chọn Level 1
+
+    // Các biến quản lý thao tác vuốt (Swipe) và Quán tính (Inertia)
+    private double mouseAnchorX;               //biến lấy vị trí con trỏ chuột trước khi kéo thả (thời điểm bắt đầu nhấn)
+    private double translateAnchorX;
+    private double velocityX = 300;
+    private double lastMouseX = 0;
+    private long lastDragTime = 0;
+    private AnimationTimer inertiaTimer;
+
+
+    // Biến quản lý nút bấm giữ
+    private AnimationTimer buttonScrollTimer;
 
     private static class LevelInfo {
         int levelNumber;
@@ -66,10 +67,14 @@ public class LevelScene {
         levelList.add(new LevelInfo(1, "LEVEL 1", "background_level_1"));
         levelList.add(new LevelInfo(2, "LEVEL 2", "background_level_2"));
         levelList.add(new LevelInfo(3, "LEVEL 3", "background_level_3"));
+        levelList.add(new LevelInfo(4, "LEVEL 4", "background_level_4"));
+        levelList.add(new LevelInfo(5, "LEVEL 5", "background_level_5"));
+        levelList.add(new LevelInfo(6, "LEVEL 6", "background_level_6"));
     }
 
     private void createLevelScene() {
         StackPane root = new StackPane();
+
 
         // 1. Nền game (Lấy nền chung của menu)
         Image bgImage = AssetManager.getImage("background_home");
@@ -82,10 +87,12 @@ public class LevelScene {
             root.setStyle("-fx-background-color: #0b131e;");
         }
 
+
         // 2. Layout tổng VBox
         VBox mainLayout = new VBox(30);
         mainLayout.setAlignment(Pos.CENTER);
-        mainLayout.setPadding(new Insets(20));
+        mainLayout.setPadding(new Insets(20));    //Đẩy mọi thứ sang phải 20 ô để card đầu tiên không dính vào tường trái, card cuối không dính tường phải
+
 
         // Tiêu đề
         Label titleLabel = new Label("CHOOSE LEVEL");
@@ -93,40 +100,135 @@ public class LevelScene {
         titleLabel.setTextFill(Color.CYAN);
         titleLabel.setEffect(new DropShadow(BlurType.GAUSSIAN, Color.rgb(0, 200, 255, 0.8), 12, 0.8, 0, 2));
 
-        // Khung chứa danh sách thẻ level (HBox hiển thị các thẻ level nằm ngang nhau)
-        levelCardsContainer = new HBox(20);
-        levelCardsContainer.setAlignment(Pos.CENTER);
+
+        // Container chứa tất cả các thẻ (bao gồm các thẻ k có trên màn hình)
+        levelCardsContainer = new HBox(220);     //spacing của mỗi thẻ là 180 ngang, 260 dọc
+        levelCardsContainer.setAlignment(Pos.CENTER_LEFT);
+
+
+
+        // Pane cardView cắt ra phần sẽ hiển thị trên màn hình của levelCardsContainer
+        Pane cardView = new Pane(levelCardsContainer);
+        cardView.setPrefSize(Main.WIDTH, 300);
+        Rectangle clip = new Rectangle(Main.WIDTH, 300);        // Cắt view cho cardView
+        cardView.setClip(clip);
+
+
+
+        //Cập nhật vị trí của levelCardContainer do bị kéo
+        cardView.setOnMousePressed(e -> {           //Hành động nhấn chuột chuẩn bị bắt đầu kéo thả
+            inertiaTimer.stop();                                   // Dừng quán tính cũ ngay khi click chuột
+            mouseAnchorX = e.getSceneX();                          // Lấy vị trí chuột thời điểm bắt đầu kéo thả
+            translateAnchorX = levelCardsContainer.getTranslateX();// Lấy vị trí levelCardContainer tại thời điểm bắt đầu kéo thả
+            lastMouseX = e.getSceneX();                            // Biến liên tục cập nhật vị trí chuột suốt thời gian kéo thả mỗi lần setOnMousePressed được gọi, nhưng để đây thì khá dễ gây nhầm lẫn với mouseAnchorX. Có thể xóa ở đây?
+            lastDragTime = System.currentTimeMillis();             // Bắt đầu tính thời điểm kéo thả, và sẽ tính lại liên tục giữa mỗi lần setOnMousePressed được gọi
+        });
+        cardView.setOnMouseDragged(e -> {           //Là loại hàm tự động cập nhật liên tục 
+            // Di chuyển HBox theo chuột
+            double deltaX = e.getSceneX() - mouseAnchorX;                                   //vị trí mới thay đối so với cũ (cụ thể là thời điểm bắt đầu giữ chuột kéo thả)
+            levelCardsContainer.setTranslateX(translateAnchorX + deltaX);                   //thay đổi vị trí của levelCardsContainer một khoảng tương tự
+            
+            // Tính toán vận tốc tức thời (velocity) sẽ sử dụng sự chênh lệch tọa độ và thời gian giữa 2 lần gọi setOnMouseDragged cuối cùng
+            long now = System.currentTimeMillis();
+            double timeDelta = (now - lastDragTime) / 1000.0; // Đổi ra giây
+            if (timeDelta > 0) {
+                velocityX = (e.getSceneX() - lastMouseX);
+            }
+            
+            //liên tục cập nhật lại vị trí chuột và thời gian từ lần gọi hàm setMouseDragged trước đó (để có thể liên tục cập nhật vận tốc tức thì cho quán tính)
+            lastMouseX = e.getSceneX();
+            lastDragTime = now;      
+
+            //Đảm bảo không kéo ra ngoài phạm vi các thẻ bài
+            clampScroll(); 
+        });
+        cardView.setOnMouseReleased(e -> {
+            // Kích hoạt quán tính bay tiếp khi nhả chuột
+            inertiaTimer.start();
+        });
+        //Cập nhật vị trí của levelCardContainer do quán tính sau khi kéo
+        inertiaTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (Math.abs(velocityX) > 0.5) {
+                    double newX = levelCardsContainer.getTranslateX() + velocityX;
+                    levelCardsContainer.setTranslateX(newX);
+                    
+                    // Ma sát (Friction) - Làm chậm dần
+                    velocityX *= 0.96; 
+                    
+                    // Giới hạn không cho văng quá đà (Clamping)
+                    clampScroll();
+                } else {
+                    velocityX = 0;
+                    this.stop(); // Tắt timer khi đã dừng hẳn để tiết kiệm CPU
+                }
+            }
+        };
+
+
         refreshLevelCards();
+
+
 
         // 3. Cụm nút bấm phía dưới (BACK và PLAY)
         HBox bottomButtons = new HBox(40);
         bottomButtons.setAlignment(Pos.CENTER);
-
         CustomButton btnBack = new CustomButton("BACK", "button_blue", () -> {
             SceneManager.getInstance().switchScene("MenuScene");
         });
-
-        // Nút PLAY với hiệu ứng hover sáng rực
         CustomButton btnPlay = new CustomButton("PLAY", "button_green", () -> {
-            // Khởi tạo PlayScene và truyền selectedLevel vào
             PlayScene playScene = new PlayScene();  //rồi chỗ này sẽ phải truyền level 
             SceneManager.getInstance().switchScene(playScene.getScene());
         });
         
+
         // Thêm hiệu ứng Hover cho 2 button
-        btnBack.setOnMouseEntered(e -> btnPlay.setEffect(new Glow(0.8)));
-        btnBack.setOnMouseExited(e -> btnPlay.setEffect(null));
+        btnBack.setOnMouseEntered(e -> btnBack.setEffect(new Glow(0.8)));
+        btnBack.setOnMouseExited(e -> btnBack.setEffect(null));
         btnPlay.setOnMouseEntered(e -> btnPlay.setEffect(new Glow(0.8)));
         btnPlay.setOnMouseExited(e -> btnPlay.setEffect(null));
 
+
+        //Ghép các layout vào với nhau tạo ra hệ thống layout hoàn chỉnh để đưa vào Scene
         bottomButtons.getChildren().addAll(btnBack, btnPlay);
-
-        mainLayout.getChildren().addAll(titleLabel, levelCardsContainer, bottomButtons);
+        mainLayout.getChildren().addAll(titleLabel, cardView, bottomButtons);
         root.getChildren().add(mainLayout);
-
         scene = new Scene(root, Main.WIDTH, Main.HEIGHT);
     }
 
+
+
+    private void clampScroll() {
+        // Chiều rộng tổng của tất cả các thẻ + spacing (nói cách khác là chiểu rộng muốn có của levelCardsContainer
+        double contentWidth = levelList.size() * 180 + (levelList.size() - 1) * 220;
+        double viewCardWidth = Main.WIDTH - 40; //vì 2 card cuối phải cách biên trái/phải 20 pixel
+
+        // Không cho phép kéo lố sang viền trái (TranslateX > 0)
+        if (levelCardsContainer.getTranslateX() > 0) {
+            levelCardsContainer.setTranslateX(0);
+            velocityX = 0; // Đập tường thì dừng quán tính
+        } 
+        // Không cho phép kéo lố sang viền phải
+        else if (levelCardsContainer.getTranslateX() < -(contentWidth - viewCardWidth)) { 
+            // Nếu độ rộng muốn có của levelCardContainer còn nhỏ hơn cửa sổ màn hình, k cho kéo
+            if (contentWidth < Main.WIDTH) {
+                levelCardsContainer.setTranslateX(0);
+            } else {
+                levelCardsContainer.setTranslateX(-(contentWidth - viewCardWidth));
+            }
+            velocityX = 0;
+        }
+    }
+
+
+
+
+
+
+
+
+    //Hàm cập nhật các thẻ
     private void refreshLevelCards() {
         levelCardsContainer.getChildren().clear();
 
